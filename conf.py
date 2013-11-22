29b7edfa8507a14f8b2d5d74500aaa5f9033e8b2
honghe <leopardsaga@gmail.com>
2013-11-22 22:38:09 +0800

    Correct and delete exlude_patterns in conf.py

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ release = '0.1'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # exclude_patterns = []
-exclude_patterns = [en/]
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
